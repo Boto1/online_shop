4286313a24a5d7aa8d40f3580573c90b3a213f50
Boto1 <olegbotsiun@gmail.com>
2019-01-03 11:27:00 +0200

    Listing of products

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module OnlineShop
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.sass.preferred_syntax = :sass
+
+    if Rails.configuration.respond_to?(:sass)
+      Rails.configuration.sass.tap do |config|
+        config.preferred_syntax = :sass
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
